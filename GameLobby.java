f576c20b4a062c433b5f722c5127608e452dbc5e
Jiakai Peng <94138989+JK1Peng@users.noreply.github.com>
2021-12-05 22:40:06 -0500

    Add files via upload

--- GameLobby.java
+++ GameLobby.java
@@ -1,4 +1,3 @@
-package Client;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -23,6 +22,7 @@ public class GameLobby extends SceneBasic{
 	private String username;				// Stores username of account trying to log in
 	private String password;				// Stores password of account trying to log in
 	private GridPane grid;					// Grid container for layout
+	private Label waitingLabel;				// Error label on the scene
 	private Socket connection;				// Socket connection to server
 	
 	public GameLobby() {
@@ -38,6 +38,10 @@ public class GameLobby extends SceneBasic{
 		
 		Button gameSettingsButton = new Button("Game Settings");
 //				 Onclick redirect the user to the settings scene
+		gameSettingsButton.setOnAction(e -> SceneManager.setGameSettingScene()); //edit by Jack
+		
+		waitingLabel = new Label("");
+		waitingLabel.setFont(new Font(30));
 		
 		grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
@@ -48,6 +52,7 @@ public class GameLobby extends SceneBasic{
 		
 		// Sets the layout in the grid
 		grid.add(buttonContain, 1, 2);
+		grid.add(waitingLabel, 1, 3);
 
 		// Vertically contains all items within the scene
 		VBox container = new VBox(30, title, grid);
@@ -66,6 +71,7 @@ public class GameLobby extends SceneBasic{
 			PrintWriter outgoing = new PrintWriter( new OutputStreamWriter(connection.getOutputStream()));
 			outgoing.println("INITG");		// Request to login that will be sent to the server
 			outgoing.flush();				// Information sent to the server
+			waitingLabel.setText("WAITING FOR OTHER PLAYERS...");
 			BufferedReader incoming = new BufferedReader( new InputStreamReader(connection.getInputStream()));
 			String runGame = incoming.readLine();
 			if(runGame.equals("READY")) {
@@ -75,5 +81,5 @@ public class GameLobby extends SceneBasic{
 			System.out.println("PLAYER READY ERROR: " + e);
 		}
 	}
-
+	
 }
